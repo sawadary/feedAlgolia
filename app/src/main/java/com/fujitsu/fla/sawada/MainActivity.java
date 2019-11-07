94b7a8bc352a236a1a29a9f6bd1a9c50b3492441
sawadary <ryo.sawada@fujitsu.com>
2019-11-07 15:53:23 -0800

    Refactored and added more comments.

--- app/src/main/java/com/fujitsu/fla/sawada/MainActivity.java
+++ app/src/main/java/com/fujitsu/fla/sawada/MainActivity.java
@@ -1,44 +1,54 @@
 package com.fujitsu.fla.sawada;
 
-import java.util.*;
-
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AbsListView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
-import com.loopj.android.http.*;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import cz.msebera.android.httpclient.Header;
 
+/**
+ * Main
+ */
 public class MainActivity extends AppCompatActivity {
 
+    //-------------------------------------------------------------------------
+    // Constants
+    //-------------------------------------------------------------------------
     private static final String ALGOLIA_QUERY = "https://hn.algolia.com/api/v1/search_by_date?tags=story&page=";
     private static final String TAG_TITLE = "title";
     private static final String TAG_CREATED_AT = "created_at";
 
-    ListView m_lstAlgoria;
-    // constrain another loading while getting data
+    //-------------------------------------------------------------------------
+    // Members
+    //-------------------------------------------------------------------------
+    /**
+     * constrain another loading while getting data
+     */
     private boolean m_isLoading = false;
 
-    ArrayList<HashMap<String, String>> m_vData = new ArrayList<HashMap<String, String>>();
+    ListView m_lstAlgoria;
+    ArrayList<HashMap<String, String>> m_vData = new ArrayList<>();
     int m_pageNo = 0;
 
+    //-------------------------------------------------------------------------
+    // Initializations
+    //-------------------------------------------------------------------------
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
         updateTitle();
 
         // Set up the listview
-        m_lstAlgoria = (ListView) this.findViewById(R.id.lstAlgolia);
+        m_lstAlgoria = this.findViewById(R.id.lstAlgolia);
         m_lstAlgoria.setOnScrollListener(new AbsListView.OnScrollListener() {
             @Override
             public void onScrollStateChanged(AbsListView view, int scrollState) {
@@ -72,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
                 if (m_isLoading)
                     return;
 
-                // If list is scrolled to the bottom, load next page
+                // If the list is scrolled to the bottom, load next page
                 if (totalItemCount == firstVisibleItem + visibleItemCount) {
                     m_pageNo++;
                     feedFromAlgolia();
@@ -88,7 +98,6 @@ public class MainActivity extends AppCompatActivity {
                 new int[]{android.R.id.text1, android.R.id.text2});
 
         m_lstAlgoria.setAdapter(adapter);
-        // End of setup for the listview
     }
 
     @Override
@@ -114,6 +123,9 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    //-------------------------------------------------------------------------
+    // Common functions
+    //-------------------------------------------------------------------------
     /**
      * Change the title in the navibar
      */
@@ -121,26 +133,34 @@ public class MainActivity extends AppCompatActivity {
         setTitle("Count:" + m_vData.size());
     }
 
+    //-------------------------------------------------------------------------
+    // Data feeds
+    //-------------------------------------------------------------------------
     /**
      * Get data from Algolia page
      */
     private void feedFromAlgolia() {
 
         m_isLoading = true;
+
+        // Feeding data from the Algolia
         final AsyncHttpClient client = new AsyncHttpClient();
         client.get(ALGOLIA_QUERY + m_pageNo, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
 
                 try {
-
                     JSONObject resp = new JSONObject(response.toString());
                     JSONArray data = resp.getJSONArray("hits");
 
                     // read data
                     for (int i = 0; i < data.length(); i++) {
                         JSONObject obj = data.getJSONObject(i);
-                        HashMap map = new HashMap();
+                        HashMap<String, String> map = new HashMap<>();
+
+                        if(!obj.has(TAG_TITLE) || !obj.has(TAG_CREATED_AT))
+                            continue;
+
                         map.put(TAG_TITLE, obj.getString(TAG_TITLE));
                         map.put(TAG_CREATED_AT, obj.getString(TAG_CREATED_AT));
                         m_vData.add(map);
